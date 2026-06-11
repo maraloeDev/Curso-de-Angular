4daab0c71b7695a060d621485d617ad39c380b22
Eduardo Martin-Sonseca <martinsonsecaeduardo@gmail.com>
2026-06-11 16:26:28 +0200

    Curso Ang2- Estructura y flujo

--- angular-app-Pildoras-Informaticas/src/app/app.ts
+++ angular-app-Pildoras-Informaticas/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  selector: 'app-root', /*Selector del componente, se usará en el index.html para cargar el componente*/
+  imports: [RouterModule], /*Importamos el RouterModule para poder usar las directivas de enrutamiento en el template del componente*/
+  templateUrl: './app.html', /*Ruta del template del componente, se puede usar una ruta relativa o absoluta*/
+  styleUrl: './app.css' /*Ruta del archivo de estilos del componente, se puede usar una ruta relativa o absoluta*/
 })
 export class App {
   protected readonly title = signal('angular-app-Pildoras-Informaticas');
+
+  saludo= "Hola, como estas"
 }
