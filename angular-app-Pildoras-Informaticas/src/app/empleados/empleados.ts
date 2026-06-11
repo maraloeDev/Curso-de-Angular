9b9d01a2f3e06e2e197abf1778ab0635c78750d8
Eduardo Martin-Sonseca <martinsonsecaeduardo@gmail.com>
2026-06-11 17:01:11 +0200

    Curso Ang3-Componente-empleados

---
+++ angular-app-Pildoras-Informaticas/src/app/empleados/empleados.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Empleado } from "../empleado/empleado";
+
+@Component({
+  selector: 'app-empleados',
+  imports: [ Empleado],
+  templateUrl: './empleados.html',
+  styleUrl: './empleados.css',
+})
+export class Empleados {}
