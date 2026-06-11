a364cb7c813f6f235e89c92e50e34f4ca029198e
Eduardo Martin-Sonseca <martinsonsecaeduardo@gmail.com>
2026-06-11 18:07:00 +0200

    Curso Ang5-Componentes II

--- angular-app-Pildoras-Informaticas/src/app/empleado/empleado.ts
+++ angular-app-Pildoras-Informaticas/src/app/empleado/empleado.ts
@@ -4,8 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-empleado',
   imports: [],
   templateUrl: './empleado.html',
-  //styleUrl: './empleado.css',
+  styleUrl: './empleado.css',
   // inline, es que si el estilo es muy pequeño, se puede poner directamente en el componente, pero si es muy grande, es mejor ponerlo en un archivo css separado
-  styles:["p{background-color: red;}"]
+  //styles:["p{background-color: red;}"]
 })
-export class Empleado {}
+export class Empleado {
+  nombre="Eduardo";
+  apellido="Martín-Sonseca";
+  edad=26;
+  empresa="Empresa X";
+
+  
+  public getEdad(){
+    return this.edad;
+  }
+  
+}
