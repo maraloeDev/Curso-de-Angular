61a042fefa2baf136ce896d09dcf3369c50eae2b
maraloeDev <martinsonsecaeduardo@gmail.com>
2026-06-20 14:43:35 +0200

    Binding bidireccional y Event Bidireccional

--- MiPrimeraApp/src/app/empleado/empleado.ts
+++ MiPrimeraApp/src/app/empleado/empleado.ts
@@ -1,29 +1,41 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-empleado',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './empleado.html',
   styleUrl: './empleado.css',
 })
 export class Empleado {
 
-  nombre="Eduardo";
+  nombre="Eduardo"; 
   apellido="Martín-Sonseca";
   edad=26;
-  //empresa="Empresa";
+  empresa="";
   habilitacionCuadro = true;
   botonRegistro=false;
-
+  textoRegistro ="No hay nadie registrado";
   
   public getEdad() {
     return this.edad;
   }
 
   /**
+   * Actualiza el mensaje según la opción seleccionada en el formulario.
+   */
+  public setUsuarioRegistrado(event: Event){
+
+    if ((<HTMLInputElement>event.target).value=="si") {
+      this.textoRegistro="El usuario se acaba de registrar"
+    } else{
+      this.textoRegistro="No hay nadie registrado";
+    }
+}
+  /**
    * funcion en el que activo el boton del registro
    */
   public getRegistroUsuario(){
-    this.botonRegistro=true
+    this.botonRegistro=false
   }
 }
