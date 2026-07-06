742bc070d6e18cd2df75eb97abf2ed4b34fb1203
Eduardo <martinsonsecaeduardo@gmail.com>
2026-07-06 20:02:29 +0200

    EJERCICIO02

--- 00-Ejercicios/02-Contador-con-botones/src/app/app.ts
+++ 00-Ejercicios/02-Contador-con-botones/src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('02-Contador-con-botones');
+  titulo = 'contador';
+  valor = 0;
+
+  btnIncremento() {
+    this.valor++;
+  }
+  
+  btnDecremento() {
+    this.valor--;
+  }
 }
